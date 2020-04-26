3c29058b1444ff91a796d43e5633e8d9f6c7212d
rekt77 <timtaeil@gmail.com>
2020-04-26 18:29:06 +0900

    confirmed answer to greedy_2217

--- boj/greedy_2217.py
+++ boj/greedy_2217.py
@@ -1,1 +1,11 @@
 #Author Rekt77
+import sys
+
+N = int(input())
+given = [int(sys.stdin.readline()) for _ in range(N)]
+given.sort(reverse=True)
+result = []
+
+for i in range(0,len(given)):
+    result.append((i+1)*given[i])
+print(max(result))
