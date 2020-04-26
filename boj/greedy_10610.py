4930c7491620982fe92112eb30dde94887957ff3
rekt77 <timtaeil@gmail.com>
2020-04-26 19:13:04 +0900

    confirmed answer to greedy_10610.py

--- boj/greedy_10610.py
+++ boj/greedy_10610.py
@@ -4,8 +4,8 @@ import sys
 N = sys.stdin.readline()[:-1]
 numStr = sorted(N,reverse=True)
 if not '0' in numStr:
-    print('-1')
-if not sum(map(int,numStr))%3==0:
-    print('-1')
-else :
+    print(-1)
+elif sum(map(int,numStr))%3==0:
     print("".join(numStr))
+else:
+    print(-1)
