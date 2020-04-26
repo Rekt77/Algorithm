8bc1fb28e7432e45297979d946ee1998f6738676
rekt77 <timtaeil@gmail.com>
2020-04-26 17:33:27 +0900

    verty first answer to greedy_5585

--- boj/greedy_5585.py
+++ boj/greedy_5585.py
@@ -1,1 +1,14 @@
 #Author Rekt77
+
+changes = [500,100,50,10,5,1]
+money2give = 1000-int(input())
+counter = 0
+for change in changes:
+    condition = money2give//change
+    if condition:
+        money2give -= (change*condition)
+        counter += condition
+    elif money2give==0:
+        break
+
+print(counter)
