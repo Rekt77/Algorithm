f65537bc85879c7db07334a1944eebe16386c5c9
rekt77 <timtaeil@gmail.com>
2022-06-02 15:22:18 +0900

    bfs로 코드변경

--- boj/16236.py
+++ boj/16236.py
@@ -1,4 +1,9 @@
-# 구현은 되지만 무조건 시간초과 나오는 코드
+# 힙을 사용하는게 핵심
+# (거리,y좌표, x좌표) 힙으로 정렬
+# dfs로는 절대 풀리지 않음
+# bfs로 풀시 먹을 수 있는 물고기가 같은 거리 2에 있을경우
+# ㄴ 상어: (0,1) 물고기1: (1,0,0), 물고기2: (1,0,2) -> 물고기 1,2를 힙으로 정렬시 (1,0,0)이 앞에옴
+#  ㄴ 물고기를 먹고 힙을 초기화, 거리 계산도 0으로 초기화하여 먹은 시점에서 부터 다른 물고기를 찾으러 bfs 하면 정답
 import sys
 from collections import deque
 import heapq
@@ -7,67 +12,44 @@ N = int(sys.stdin.readline())
 aquarium = [list(map(int,sys.stdin.readline().strip().split())) for _ in range(N)]
 sharkSize = 2
 
-# 힙에 먹을 수 있는 물고기 좌표 다 넣는다
-# ㄴ 좌표를 넣을 때 (거리,(tarY,tarX))
-# 위에서 하나씩 꺼내고 갈 수 있는 지 체크
-# 갈 수 있으면 sharkSize += 1, curLoc = (tarY,tarX)
-def sharkAndtarget(shark,aq):
-    target = []
+def sharkFinder(aq):
     for i in range(len(aq)):
         for j in range(len(aq)):
             if aq[i][j] == 9:
                 loc = (i,j)
-            if aq[i][j] != 9 and 0<aq[i][j]<shark:
-                target.append((i,j))
-    return loc,target
-
-#dfs
-def distanceCheck(curX,curY,tarX,tarY,visited,sharkSize):
-    if (tarX,tarY) == (curX,curY):
-        return 1
-
-    for vy,vx in [(0,1),(1,0),(0,-1),(-1,0)]:
-        next_curX = curX+vx
-        next_curY = curY+vy
-        if 0<=next_curX<N and 0<=next_curY<N and (visited[next_curY][next_curX] == False or visited[next_curY][next_curX]>visited[curY][curX]+1):
-            if aquarium[next_curY][next_curX] <= sharkSize:
-                visited[next_curY][next_curX] = visited[curY][curX] + 1
-                distanceCheck(next_curX,next_curY,tarX,tarY,visited,sharkSize)
-
-    return visited[tarY][tarX]
+                aq[i][j] = 0
+                return loc
 
 visited = [[False]*N for _ in range(N)]
-
+c_visited = copy.deepcopy(visited)
+priority = []
 time = 0
 counter = 0
-cur, target =sharkAndtarget(sharkSize,aquarium)
+cur = sharkFinder(aquarium)
+heapq.heappush(priority,(0,cur[0],cur[1]))
 
-while target:
-    priority = []
-    for i in range(len(target)):
-        c_visited = copy.deepcopy(visited)
-        c_visited[cur[0]][cur[1]] = 1
-        distance = distanceCheck(cur[1],cur[0],target[i][1],target[i][0],c_visited,sharkSize)
-        if distance:
-            #거리 , y , x
-            heapq.heappush(priority,(distance-1, target[i][0], target[i][1]))
-    if not priority:
-        break
-    else :
-        dead=heapq.heappop(priority)
-        time += dead[0]
-        #상어위치-물고기 위치 스왑
-        aquarium[cur[0]][cur[1]] = 0
-        aquarium[dead[1]][dead[2]] = 9
+while priority: 
+    distance, curY, curX = heapq.heappop(priority)
+    c_visited[curY][curX] = True
+
+    if 0<aquarium[curY][curX]<sharkSize:
+        aquarium[curY][curX] = 0
+        time += distance
         counter += 1
+
         if counter==sharkSize:
             sharkSize+=1
             counter = 0
-        cur, target = sharkAndtarget(sharkSize,aquarium)
-
-#    for each in aquarium:
-#        print(each)
-    
-#    print("")
+        distance = 0
+        priority = []
+        c_visited = copy.deepcopy(visited)
+        
+    for vy,vx in [(0,1),(1,0),(0,-1),(-1,0)]:
+        next_curX = curX+vx
+        next_curY = curY+vy
+        if 0<=next_curX<N and 0<=next_curY<N:
+            if aquarium[next_curY][next_curX] <= sharkSize and c_visited[next_curY][next_curX] == False:
+                c_visited[next_curY][next_curX] = True
+                heapq.heappush(priority,(distance+1, next_curY, next_curX))
 
 print(time)
